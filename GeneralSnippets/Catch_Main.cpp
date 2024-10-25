b243ecf6497a075013fae0163d5bf8654fd21ddf
Peter Loos <peter.loos@gmx.de>
2024-10-25 15:01:33 +0200

    Stand Freitag 15 Uhr.

---
+++ GeneralSnippets/Catch_Main.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include <iostream>
+
+#include <Catch2/catch.hpp>
+
+//int main()
+//{
+//    std::cout << "Hello Catch2" << std::endl;
+//    return 0;
+//}
